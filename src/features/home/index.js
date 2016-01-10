d98db31c155d6dbfdb7e542efad930f77baa366c
alifakheri <fakheri.ali@gmail.com>
2016-01-10 05:51:01 +0100

    Wraps home around main layout with a shared header

--- src/features/home/index.js
+++ src/features/home/index.js
@@ -5,8 +5,8 @@ import uirouter from 'angular-ui-router';
 
 import routing from './home.routes';
 import HomeController from './home.controller';
-import randomNames from '../../services/randomNames.service';
-import greeting    from '../../directives/greeting.directive';
+import randomNames from '../../common/services/randomNames.service';
+import greeting    from '../../common/directives/greeting.directive';
 
 export default angular.module('app.home', [uirouter, randomNames, greeting])
   .config(routing)
